56eb0677119b87286db1d156d4a8c9159f9737a2
Fer <pher.gm@gmail.com>
2014-04-25 02:45:55 +0200

    added setOnclickListenet to the button

--- src/com/example/crystalball/MainActivity.java
+++ src/com/example/crystalball/MainActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -15,8 +16,19 @@ public class MainActivity extends ActionBarActivity {
         setContentView(R.layout.activity_main);
         
         //Declare our View variables and assign them the Views from the layout file
-        TextView answerLabel = (TextView) findViewById(R.id.textView1);
+        final TextView answerLabel = (TextView) findViewById(R.id.textView1);
         Button getAnswerButton = (Button)findViewById(R.id.button1);
+        
+        getAnswerButton.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View arg0) {
+				// The button was clicked, so update the answer label with an answer
+				String answer = "Yes";
+				answerLabel.setText(answer);
+			}
+		});
+        
     }
 
 
