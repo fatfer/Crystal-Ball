3d016152f45556bd50209ad6c4032541e05dc475
Fer <pher.gm@gmail.com>
2014-04-27 17:30:13 +0200

    added AlphaAnimation to the answer text and refactoring

--- src/com/example/crystalball/MainActivity.java
+++ src/com/example/crystalball/MainActivity.java
@@ -6,43 +6,57 @@ import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.animation.AlphaAnimation;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 public class MainActivity extends ActionBarActivity {
+	
+	private TextView mAnswerLabel;
+	private Button mGetAnswerButton;
+	private ImageView mCrystalBallImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         
-        //Declare our View variables and assign them the Views from the layout file
-        final TextView answerLabel = (TextView) findViewById(R.id.textView1);
-        Button getAnswerButton = (Button)findViewById(R.id.button1);
+        //Assign the Views from the layout file
+        mAnswerLabel = (TextView) findViewById(R.id.textView1);
+        mGetAnswerButton = (Button)findViewById(R.id.button1);
+    	mCrystalBallImage = (ImageView) findViewById(R.id.imageView1);
         
-        getAnswerButton.setOnClickListener(new View.OnClickListener() {
+        mGetAnswerButton.setOnClickListener(new View.OnClickListener() {
 			
 			@Override
 			public void onClick(View arg0) {
 				// The button was clicked, so update the answer label with an answer
 				String answer = "Yes";
-				answerLabel.setText(answer);
+				mAnswerLabel.setText(answer);
 				
 				animateCrystalBall();
+				animateAnswer();
 			}
 		});
         
     }
     
     private void animateCrystalBall(){
-    	ImageView crystalBallImage = (ImageView) findViewById(R.id.imageView1);
-    	crystalBallImage.setImageResource(R.drawable.ball_animation);
-    	AnimationDrawable ballAnimation = (AnimationDrawable)crystalBallImage.getDrawable();
+    	mCrystalBallImage.setImageResource(R.drawable.ball_animation);
+    	AnimationDrawable ballAnimation = (AnimationDrawable)mCrystalBallImage.getDrawable();
     	if(ballAnimation.isRunning()) {
     		ballAnimation.stop();
     	}
     	ballAnimation.start();
+    }
+    
+    private void animateAnswer(){
+    	AlphaAnimation fadeInAnimation = new AlphaAnimation(0, 1);
+    	fadeInAnimation.setDuration(1500);
+    	fadeInAnimation.setFillAfter(true);
+    	mAnswerLabel.setAnimation(fadeInAnimation);
+    
     }
 
 
